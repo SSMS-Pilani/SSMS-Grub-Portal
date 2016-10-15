c27a7821df0f69b721f002c37cc93049a23fb3e3
rtgdk <rohit.lodhartg@gmail.com>
2016-10-15 23:15:07 +0530

    mysql

--- project/settings.py
+++ project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+	'USER' : 'myprojectuser',
+	'PASSWORD' : 'rango',
+	'HOST' : 'localhost',
+	'PORT' : '',
     }
 }
 
