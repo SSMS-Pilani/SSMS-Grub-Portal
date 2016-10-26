ea968bb26ced5deb8fd2f6f769ab7c470ac881cb
rtgdk <rohit.lodhartg@gmail.com>
2016-10-26 11:56:41 +0530

    demo

--- project/settings.py
+++ project/settings.py
@@ -81,20 +81,18 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-	'ENGINE' : 'django.db.backends.sqlite3',
-	'NAME' : 'my.db',
-	}
-	
-}
-"""
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dr8aqo5iubm3t',
 	'USER' : 'jejbloegqrfbph',
 	'PASSWORD' : 'qr_vxBeNnpe7jAy43DwdArVYiB',
 	'HOST' : 'ec2-54-243-201-3.compute-1.amazonaws.com',
 	'PORT' : '5432',
 	
-    }"""
+    }
+	
+}
+"""
+        """
 """"""
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
