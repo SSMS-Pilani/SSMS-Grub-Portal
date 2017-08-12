a6f78a030a825a5a6ed3efafa1b767fbbb6ebb55
Ishan-sopho <f2016075@pilani.bits-pilani.ac.in>
2017-08-12 12:03:12 +0530

    added meal and item models

--- ssms/models.py
+++ ssms/models.py
@@ -147,3 +147,17 @@ class Feedback(models.Model):
 	def __unicode__(self):
 		return self.gm_id.name + "-" + self.stugm_id
 
+
+class Meal(models.Model):
+    date = models.DateField(null=False, blank=False)
+    meal_type = models.CharField(max_length=30, choices=(('grub','GRUB'),('lunch', 'LUNCH') , ('dinner','DINNER'), ('breakfast','BREAKFAST')))
+    day = models.CharField(max_length=10, null=True)
+    def __unicode__(self):
+        return str(self.date) + str(self.meal_type)
+
+
+class Items(models.Model):
+    item = models.CharField(null=False, blank=False, max_length=30)
+    meal = models.ForeignKey('Meal')
+    def __unicode__(self):
+        return str(self.item) + str(self.meal.date)
